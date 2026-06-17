7847db37fcbe9eac46435521ff79dbe657076b53
Matias <ma.imil@duocuc.cl>
2026-06-16 23:48:17 -0400

    Aumenta capacidad EKS para despliegue

--- infra/eks.tf
+++ infra/eks.tf
@@ -18,6 +18,6 @@ resource "aws_eks_node_group" "workers" {
     max_size     = 3
     min_size     = 2
   }
-  instance_types = ["t2.micro"]
+  instance_types = ["t3.medium"]
   capacity_type  = "ON_DEMAND"
 }
